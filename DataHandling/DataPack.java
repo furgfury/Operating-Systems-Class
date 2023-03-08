c5c5b59c928d9a01d99056e978bbde176f01f687
Quinn <46946004+furgfury@users.noreply.github.com>
2023-03-07 20:05:46 -0500

    new way to handle PCB data

--- DataHandling/DataPack.java
+++ DataHandling/DataPack.java
@@ -6,18 +6,58 @@ public class DataPack
 
     int[] intData = new int[NUMBER_OF_INT_VARS];
     String[] strData = new String[NUMBER_OF_STR_VARS];
+    int cpuRequired = 0;
+    int arrivalTime = 0;
+
+    public String[] totalData = new String[12];
 
     public DataPack(String[] data)
     {
-        this.intData = getInts(data);
-        this.strData = getStr(data);
+        this.intData = getInts(data, 0, NUMBER_OF_INT_VARS);
+        this.strData = getStr(data, NUMBER_OF_INT_VARS, NUMBER_OF_STR_VARS);
+        
+        this.cpuRequired = Integer.parseInt(data[10]);
+        this.arrivalTime = Integer.parseInt(data[11]);
+
+        totalData = consolidateData();
     }
 
-    private int[] getInts(String[] data)
+    public String getDataString()
+    {
+        String temp = "";
+        
+        for(int j = 0; j < totalData.length; j++)
+        {
+            temp += totalData[j] + " ";
+        }
+        return temp;
+    }
+
+    private String[] consolidateData()
+    {
+        String[] temp = new String[12];
+
+        for(int i = 0; i < intData.length; i++)
+        {
+            temp[i] = intData[i] + "";
+        }
+
+        for(int i = 0; i < strData.length; i++)
+        {
+            temp[i + intData.length] = strData[i];
+        }
+
+        temp[10] = cpuRequired + "";
+        temp[11] = arrivalTime + "";
+
+        return temp;
+    }
+
+    private int[] getInts(String[] data, int start, int end)
     {
         int[] temp = new int[NUMBER_OF_INT_VARS];
 
-        for(int i = 0; i < NUMBER_OF_INT_VARS; i++)
+        for(int i = start; i < end; i++)
         {
             temp[i] = Integer.parseInt(data[i]);
         }
@@ -25,11 +65,11 @@ public class DataPack
         return temp;
     }
 
-    private String[] getStr(String[] data)
+    private String[] getStr(String[] data, int start, int end)
     {
         String[] temp = new String[NUMBER_OF_STR_VARS];
 
-        for(int i = NUMBER_OF_INT_VARS; i < NUMBER_OF_INT_VARS + NUMBER_OF_STR_VARS; i++)
+        for(int i = start; i < end; i++)
         {
             temp[i] = data[i];
         }
